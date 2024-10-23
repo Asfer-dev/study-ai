01340f3065f2a46a4b29fe24907c632a56eb4d5f
Asfer Ali <asferali004@gmaill.com>
2024-10-24 04:19:10 +0500

    updated '/classrooms/[id]/layout.tsx

--- src/app/classrooms/[id]/loading.tsx
+++ src/app/classrooms/[id]/loading.tsx
@@ -1,32 +1,11 @@
 import React from "react";
-import Skeleton from "react-loading-skeleton";
 
 interface ClassroomLayoutProps {
   children: React.ReactNode;
 }
 
 const ClassroomLayout = ({ children }: ClassroomLayoutProps) => {
-  return (
-    <div className="flex-1 justify-between flex flex-col h-full">
-      <div
-        id="posts"
-        className="flex h-full flex-1 flex-col-reverse gap-5 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
-      >
-        <div className="w-[80%]">
-          <Skeleton height={50} containerClassName="flex-1" />
-        </div>
-        <div className="w-[80%]">
-          <Skeleton height={50} containerClassName="flex-1" />
-        </div>
-        <div className="w-[80%]">
-          <Skeleton height={50} containerClassName="flex-1" />
-        </div>
-      </div>
-      <div className="mt-4 border-t border-zinc-200 dark:border-zinc-700 px-4 pt-4 mb-2 sm:mb-0">
-        <Skeleton height={100} />
-      </div>
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default ClassroomLayout;
